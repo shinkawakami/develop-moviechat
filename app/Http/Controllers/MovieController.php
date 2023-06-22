09d8feaf5c2ca56fca5b89384a1c85e4a9aa7c1e
shinkawakami <snnsk.2002@gmail.com>
2023-06-23 00:17:46 +0900

    Add page

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -9,6 +9,22 @@ class MovieController extends Controller
 {
     public function index(Movie $movies)
     {
-        return $movies->get();
+        return view('movies.index')->with(['pages' => $movies->getByLimit()]);  
+       //blade内で使う変数'pages'と設定。'pages'の中身にgetを使い、インスタンス化した$moiesを代入。
+    }
+    
+    public function make(Movie $movies)
+    {
+        return view('movies.make');
+    }
+    
+    public function search(Movie $movies)
+    {
+        return view('movies.search');
+    }
+    
+    public function showlist(Movie $movies)
+    {
+        return view('movies.showlist');
     }
 }
