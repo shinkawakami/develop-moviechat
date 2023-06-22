8dd67e0f0e0683103c478d8fb19a1b412d18f52b
shinkawakami <snnsk.2002@gmail.com>
2023-06-23 00:17:46 +0900

    Add page

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
+    
+    public function getByLimit(int $limit_count = 10)
+    {
+        return $this->orderby('updated_at', 'DESC')->limit($limit_count)->get();
+    }
 }
