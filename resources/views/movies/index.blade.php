1db134ecd1487172475c6befc6c6c209d3509b8e
shinkawakami <snnsk.2002@gmail.com>
2023-06-25 20:52:40 +0900

    Add Breeze

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -7,25 +7,23 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>Homepage</h1>
-        <div class='button'>
-            <div class='group_make_page'>
-                <a href="/movies/make">グループ作成</a>
-            </div>
-            <div class='group_search_page'>
-                <a href="/movies/search">グループ検索</a>
-            </div>
-            <div class='group_showlist_page'>
-                <a href="/movies/showlist">グループ一覧</a>
-            </div>
-        </div>
-        <div class='posts'>
-            @foreach ($pages as $page)
-                <div class='post'>
-                    <h2 class='title'>{{ $page->title }}</h2>
-                    <p class='body'>{{ $page->body }}</p>
+        <x-app-layout>
+            <x-slot name="header">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    Make Group
+                </h2>
+            </x-slot>
+            <div class='button'>
+                <div class='group_make_page'>
+                    <a href="/movies/make">グループ作成</a>
                 </div>
-            @endforeach
-        </div>
+                <div class='group_search_page'>
+                    <a href="/movies/search">グループ検索</a>
+                </div>
+                <div class='group_showlist_page'>
+                    <a href="/movies/showlist">グループ一覧</a>
+                </div>
+            </div>
+        </x-app-layout>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 
@@ -9,16 +10,27 @@ use App\Http\Controllers\MovieController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', [MovieController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/movies/make', [MovieController::class ,'make']);
+Route::controller(MovieController::class)->middleware(['auth'])->group(function(){
+    Route::get('/', 'index')->name('index');
+    Route::get('/movies/make', 'make')->name('make');
+    Route::post('/movies', 'store')->name('store');
+    Route::get('/movies/search', 'search')->name('search');
+    Route::get('/movies/showlist', 'showlist')->name('showlist');
+});
 
-Route::get('/movies/search', [MovieController::class ,'search']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/movies/showlist', [MovieController::class ,'showlist']);
-
+require __DIR__.'/auth.php';
