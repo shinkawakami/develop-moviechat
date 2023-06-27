cc5d528c14b66c1654baeeec81381ec52270ba92
shinkawakami <snnsk.2002@gmail.com>
2023-06-27 13:57:27 +0900

    Add group list page

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -22,7 +22,7 @@ class Group extends Model
 
     public function users()
     {
-        return $this->belongsToMany(User::class, 'group_user', 'group_id', 'user_id');
+        return $this->belongsToMany(User::class, 'group_users', 'group_id', 'user_id');
     }
     
     public function movie()
