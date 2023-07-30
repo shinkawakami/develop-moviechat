bd4205438c78e8703dce229cdbd620d4d27fc695
shinkawakami <snnsk.2002@gmail.com>
2023-07-30 16:50:52 +0900

    Fix migration

--- database/migrations/2023_06_29_194831_create_group_user_table.php
+++ database/migrations/2023_06_29_194831_create_group_user_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('group_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
