0acfa37deb99f2116d263f624c9fa53498da2fd6
shinkawakami <snnsk.2002@gmail.com>
2023-08-01 16:16:12 +0900

    Fix pusher

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,36 +1,51 @@
-// Pusherの初期化
-const pusher = new Pusher(window.PUSHER_APP_KEY, {
-  cluster: window.PUSHER_APP_CLUSTER,
-  encrypted: true
-});
+// グループIDの取得
+const linkElement = document.querySelector('a[href^="{{ route('groups.show') }}"]');
+const groupId = linkElement.href.split('/').pop();
 
-// このユーザーが現在のグループに参加しているチャンネルを購読
-var channel = pusher.subscribe('group.' + window.CURRENT_GROUP_ID);
+// '/get-data' からのデータの取得
+fetch('/moviechat/groups/receive')
+    .then(response => response.json())
+    .then(data => {
+        authId = data.auth_id;
+        csrfToken = data.csrf_token;
+        pusherAppKey = data.pusher_app_key;
+        pusherAppCluster = data.pusher_app_cluster;
 
-channel.bind('App\\Events\\MessageSent', function(data) {
-    var deleteButton = '';
-    if (data.message.user_id == window.AUTH_ID) {  
-        deleteButton = `
-        <form action="/moviechat/groups/${window.CURRENT_GROUP_ID}/chats/${data.message.id}" method="POST" class="delete-form">
-            <input type="hidden" name="_token" value="${window.CSRF_TOKEN}">  
-            <input type="hidden" name="_method" value="DELETE">
-            <button class="delete-message" type="submit">削除</button>
-        </form>
-        `;
-    }
-    
-    var messageElement = `
-        <div class="message-item">
-            <div class="message-content">
-                <img src="${data.message.user.image_url}" alt="Profile Image" class="rounded-icon">
-                <span>${data.message.user.name}: ${data.message.content}</span>
-            </div>
-            <div class="message-time">
-                ${data.message.created_at}
-                ${deleteButton}
-            </div>
-        </div>
-    `;
-    
-    document.querySelector(".content").innerHTML += messageElement;
-});
+        // Pusherの初期化
+        const pusher = new Pusher(pusherAppKe, {
+            cluster: pusherAppCluster,
+            encrypted: true
+        });
+
+        // このユーザーが現在のグループに参加しているチャンネルを購読
+        const channel = pusher.subscribe('group.' + groupId);
+
+        channel.bind('App\\Events\\MessageSent', function(data) {
+            let deleteButton = '';
+            if (data.message.user_id == authId) {
+                deleteButton = `
+                    <form action="/moviechat/groups/${groupId}/chats/${data.message.id}" method="POST" class="delete-form">
+                        <input type="hidden" name="_token" value="${csrfToken}">  
+                        <input type="hidden" name="_method" value="DELETE">
+                        <button class="delete-message" type="submit">削除</button>
+                    </form>
+                `;
+            }
+
+            const messageElement = `
+                <div class="message-item">
+                    <div class="message-content">
+                        <img src="${data.message.user.image_url}" alt="Profile Image" class="rounded-icon">
+                        <span>${data.message.user.name}: ${data.message.content}</span>
+                    </div>
+                    <div class="message-time">
+                        ${data.message.created_at}
+                        ${deleteButton}
+                    </div>
+                </div>
+            `;
+
+            document.querySelector(".content").innerHTML += messageElement;
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
