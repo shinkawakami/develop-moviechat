635fa4e2c5d764e87976dfa8859a6230799ee95e
shinkawakami <snnsk.2002@gmail.com>
2023-08-02 15:10:13 +0900

    Fix design

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -33,7 +33,7 @@
                         </div>
                     </form>
         
-                    <form class="box">
+                    <div class="box">
                         <h2 class="subtitle movie-search-title">映画検索</h2>
                         <div class="field has-addons">
                             <div class="control is-expanded">
@@ -43,9 +43,11 @@
                                 <button id="search-btn" class="button is-info">検索</button>
                             </div>
                         </div>
-                    </form>
-        
-                    <div id="search-results"></div>
+                    
+                        <div id="search-results">
+                            <!-- 映画の検索結果はここに表示されます -->
+                        </div>
+                    </div>
                 </div>
             </section>
         </x-app-layout>
