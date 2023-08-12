e3a11b3822df46558800f97e630f4d94d29605f2
shinkawakami <snnsk.2002@gmail.com>
2023-08-12 11:49:21 +0900

    Fix migration

--- database/migrations/2023_07_06_131431_create_viewing_user_table.php
+++ database/migrations/2023_07_06_131431_create_viewing_user_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('approvers', function (Blueprint $table) {
+        Schema::create('viewing_user', function (Blueprint $table) {
             $table->id();
             $table->foreignId('viewing_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('approvers');
+        Schema::dropIfExists('viewing_user');
     }
 };
