187281c9dbbac12b67a6aa82aa67035eeef28c22
shinkawakami <snnsk.2002@gmail.com>
2023-08-30 15:02:32 +0900

    Add movie search

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,51 +4,86 @@ namespace App\Http\Controllers;
 
 use App\Models\Movie;
 use App\Http\Requests\Movie\SearchRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use GuzzleHttp\Client;
 
 class MovieController extends Controller
 {
+    private $apiKey;
+    private $baseApiUrl;
+
+    public function __construct()
+    {
+        $this->apiKey = config('tmdb.api_key');
+        $this->baseApiUrl = 'https://api.themoviedb.org/3';
+    }
+
     // 映画一覧画面の表示
     public function index()
     {
-        $apiKey = config('tmdb.api_key');
-        $response = Http::get("https://api.themoviedb.org/3/movie/popular?api_key={$apiKey}&language=ja-JP&page=1");
-        $popularMovies = $response->json()['results'] ?? [];
+        $popularMoviesResponse = Http::get("{$this->baseApiUrl}/movie/popular", [
+            'api_key' => $this->apiKey,
+            'language' => 'ja-JP',
+            'page' => 1
+        ]);
 
-        return view('movies.index', compact('popularMovies'));
+        $genresResponse = Http::get("{$this->baseApiUrl}/genre/movie/list", [
+            'api_key' => $this->apiKey,
+            'language' => 'ja-JP'
+        ]);
+        
+        $popularMovies = $popularMoviesResponse->json()['results'] ?? [];
+        $genres = $genresResponse->json()['genres'];
+        
+
+        return view('movies.index', compact('popularMovies', 'genres'));
     }
         
     // 映画検索の処理（JSONで返す）
-    public function search(SearchRequest $request)
+    public function search(Request $request)
     {
-        $validatedData = $request->validated();
+        $query = $request->input('query');
+        $genres = $request->input('genres');
+        $startYear = $request->input('startYear');
+        $endYear = $request->input('endYear');
+        $page = $request->input('page', 1);
         
-        $query = $validatedData['query'];
-        $page = $validatedData['page'] ?? 1;
-    
-        $client = new Client();
-        $response = $client->get('https://api.themoviedb.org/3/search/movie', [
-            'query' => [
-                'api_key' => config('tmdb.api_key'),
-                'query' => $query,
-                'page' => $page,
-                'language' => 'ja-JP'
-            ]
-        ]);
-    
-        $movieData = json_decode($response->getBody(), true);
-    
-        return response()->json($movieData);
+        $parameters = [
+            'api_key' => $this->apiKey,
+            'language' => 'ja-JP',
+            'query' => $query,
+            'page' => $page
+        ];
+        
+        if ($genres) {
+            $parameters['with_genres'] = $genres;
+        }
+
+        if ($startYear && $endYear) {
+            $parameters['primary_release_date.gte'] = "{$startYear}-01-01";
+            $parameters['primary_release_date.lte'] = "{$endYear}-12-31";
+        }
+
+        if (!$query && ($genres || $startYear || $endYear)) {
+            $searchResponse = Http::get("{$this->baseApiUrl}/discover/movie", $parameters);
+        } else {
+            $searchResponse = Http::get("{$this->baseApiUrl}/search/movie", $parameters);
+        }
+                
+        return response()->json($searchResponse->json());
     }
        
     // 映画詳細画面の表示
     public function show($tmdb_id)
     {
-        $apiKey = config('tmdb.api_key');
-        $response = Http::get("https://api.themoviedb.org/3/movie/{$tmdb_id}?api_key={$apiKey}&language=ja-JP");
+        $response = Http::get("{$this->baseApiUrl}/movie/{$tmdb_id}", [
+            'api_key' => $this->apiKey,
+            'language' => 'ja-JP'
+        ]);
+        
         $movieData = $response->json();
-    
+        
         $movie = Movie::updateOrCreateFromTMDB($movieData)->load(['groups', 'posts']);
         
         return view('movies.show', compact('movie'));
