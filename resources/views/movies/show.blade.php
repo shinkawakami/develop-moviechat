f45710c4b89de6f7da4cf22aa7db9343b104c459
shinkawakami <snnsk.2002@gmail.com>
2023-08-31 00:51:02 +0900

    Fix movie show

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <title>MovieChat - DetailMovie</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="{{ asset('css/movies/show.css') }}" rel="stylesheet">
 </head>
 
@@ -14,18 +15,71 @@
             <div class="container">
                 <div class="header-flex">
                     <h1 class="title">映画詳細</h1>
-                    <a class="return-button" href="{{ route('movies.index') }}">戻る</a>
                 </div>
                 
                 <div class="box">
-                    <h2 class="subtitle">{{ $movie->title }}</h2>
-                    @if(!$movie->posts->isEmpty())
-                        <p>
-                            評価：{{ $movie->averageRating() }}
-                        </p>
-                    @endif
+                    <div class="columns">
+                        <div class="column is-two-thirds">
+                            <h1 class="title movie-title">{{ $movie->title }}</h1>
+                            
+                            @if(!$movie->posts->isEmpty())
+                                <div class="movie-info rating-info">
+                                    <span class="icon-container"><i class="fas fa-star"></i></span>
+                                    {{ $movie->averageRating() }}
+                                </div>
+                            @endif
+                            
+                            @if(isset($movieData['release_date']))
+                                <div class="movie-info">
+                                    <span class="icon-container"><i class="fas fa-calendar-alt"></i></span>
+                                    {{ $movieData['release_date'] }}
+                                </div>
+                            @endif
+                            
+                            @if(isset($movieData['genres']) && !empty($movieData['genres']))
+                                <div class="movie-info">
+                                    <span class="icon-container"><i class="fas fa-tag"></i></span>
+                                    @foreach ($movieData['genres'] as $genre)
+                                        {{ $genre['name'] }}　
+                                    @endforeach
+                                </div>
+                            @endif
+                            
+                            @if(isset($movieData['runtime']))
+                                <div class="movie-info">
+                                    <span class="icon-container"><i class="fas fa-clock"></i></span>
+                                    {{ $movieData['runtime'] }} 分
+                                </div>
+                            @endif
+                            
+                            @if(isset($movieData['overview']))
+                                <div class="movie-info">
+                                    <span class="icon-container"><i class="fas fa-info-circle"></i></span>
+                                    {{ $movieData['overview'] }}
+                                </div>
+                            @endif
+                            
+                            @if(isset($movieData['videos']['results']) && !empty($movieData['videos']['results']))
+                                <div class="movie-info">
+                                    <span class="icon-container"><i class="fas fa-film"></i></span>
+                                    映画予告
+                                </div>
+                                @foreach ($movieData['videos']['results'] as $video)
+                                    @if ($video['type'] == "Trailer")
+                                        <a class="button is-link video-link" href="https://www.youtube.com/watch?v={{ $video['key'] }}" target="_blank">{{ $video['name'] }}</a>
+                                    @endif
+                                @endforeach
+                            @endif
+                        </div>
+                
+                        <div class="column">
+                            @if(isset($movieData['poster_path']))
+                                <img src="https://image.tmdb.org/t/p/w500{{ $movieData['poster_path'] }}" alt="{{ $movieData['title'] }}">
+                            @endif
+                        </div>
+                    </div>
                 </div>
-
+                
                 <div class="box">
                     <h2 class="subtitle">関連するグループ</h2>
                     @if(!$movie->groups->isEmpty())
