0e95ead36f097cc416deee29dc3ec26b6797ce20
kawakami25 <shinnosuke.kawakami@g.dreamcareer.co.jp>
2025-08-30 20:54:55 +0900

    本番環境でHTTPSを強制する機能を追加

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     {
         // \URL::forceScheme('https');
         // $this->app['request']->server->set('HTTPS','on');
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
